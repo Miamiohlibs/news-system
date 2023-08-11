fd5bf038f31a2bbae4764504171ff4373a056fce
Meng-MiamiOH <43998638+Meng-MiamiOH@users.noreply.github.com>
2023-08-11 13:24:26 -0400

    missing Miami Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily:{
+        'inter': ['Inter', 'sans-serif'],
+        'satoshi': ['Satoshi', 'sans-serif'],
       },
+      colors: {
+        'miami-red-primary': '#C41230',
+        'miami-red-secondary': '#AD102A',
+        'miami-tan-dark': '#CCCAB8',
+        'miami-tan-light': '#EDECE2',
+        'miami-gold': '#EFDB72',
+        'miami-warm-white': '#FAF9F7',
+      }
     },
   },
   plugins: [],
